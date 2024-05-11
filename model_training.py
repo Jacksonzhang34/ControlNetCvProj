47ba45a733529ed8a8e3fb0c83aca0031dd6384e
Jacksonzhang34 <63770954+Jacksonzhang34@users.noreply.github.com>
2024-05-11 16:05:14 -0400

    arg parse for running the code

--- model_training.py
+++ model_training.py
@@ -1,22 +1,34 @@
-import torch
-import sys
-import os
-import torchvision
-import json
+import argparse
 import cv2
+import json
 import numpy as np
-from torch.utils.data import Dataset
-import pytorch_lightning as pl
+import os
+import torch
+from torch.utils.data import DataLoader, Dataset
 from torch.utils.data import DataLoader
 from cldm.logger import ImageLogger
 from cldm.model import create_model, load_state_dict
+import pytorch_lightning as pl
 from pytorch_lightning.callbacks import ModelCheckpoint
 
+# Parse command-line arguments
+def parse_args():
+    current_dir = os.getcwd()
+    parser = argparse.ArgumentParser(description="Train a model on a custom dataset")
+    parser.add_argument('--data_dir', type=str, default=os.path.join(current_dir, 'datasets/training/mini/'), help='Directory containing the data')
+    parser.add_argument('--weights_dir', type=str, default=os.path.join(current_dir, 'models/trained weights/'), help='Directory to save the model weights')
+    parser.add_argument('--resume_path', type=str, default=os.path.join(current_dir, 'models/control_sd21_ini.ckpt'), help='Path to resume model training')
+    parser.add_argument('--checkpoint_name', type=str, default='mini_weights', help='Base name for the saved model checkpoint')
+    return parser.parse_args()
 
+args = parse_args()
+
+# Dataset class
 class MyDataset(Dataset):
-    def __init__(self):
+    def __init__(self, data_dir):
+        self.data_dir = data_dir
         self.data = []
-        with open('/content/drive/My Drive/CV-JV-final-project/datasets/training/mini/prompt.json', 'rt') as f:
+        with open(os.path.join(data_dir, 'prompt.json'), 'rt') as f:
             for line in f:
                 self.data.append(json.loads(line))
 
@@ -25,59 +37,34 @@ class MyDataset(Dataset):
 
     def __getitem__(self, idx):
         item = self.data[idx]
-
         source_filename = item['source']
         target_filename = item['target']
         prompt = item['prompt']
-
-        source = cv2.imread('/content/drive/My Drive/CV-JV-final-project/datasets/training/mini/' + source_filename)
-        target = cv2.imread('/content/drive/My Drive/CV-JV-final-project/datasets/training/mini/' + target_filename)
-
-        # Do not forget that OpenCV read images in BGR order.
+        source = cv2.imread(os.path.join(self.data_dir, source_filename))
+        target = cv2.imread(os.path.join(self.data_dir, target_filename))
         source = cv2.cvtColor(source, cv2.COLOR_BGR2RGB)
         target = cv2.cvtColor(target, cv2.COLOR_BGR2RGB)
-
-        # Normalize source images to [0, 1].
         source = source.astype(np.float32) / 255.0
-
-        # Normalize target images to [-1, 1].
         target = (target.astype(np.float32) / 127.5) - 1.0
-
         return dict(jpg=target, txt=prompt, hint=source)
-    
 
+# Callback for model checkpoint
 checkpoint_callback = ModelCheckpoint(
-    dirpath='/content/drive/My Drive/CV-JV-final-project/models/trained weights/',  # Directory to save the model
-    filename='mini_weights',
+    dirpath=args.weights_dir,
+    filename=args.checkpoint_name,
     save_top_k=1,
     verbose=True
 )
 
-
-resume_path = '/content/drive/My Drive/CV-JV-final-project/models/control_sd21_ini.ckpt'
-batch_size = 4
-logger_freq = 300
-learning_rate = 1e-5
-sd_locked = True
-only_mid_control = False
-
-
-# First use cpu to load models. Pytorch Lightning will automatically move it to GPUs.
+# Model preparation (assuming create_model function exists and works as expected)
 model = create_model('./models/cldm_v21.yaml').cpu()
-# model.load_state_dict(load_state_dict(resume_path, location='cpu'))
-model.load_state_dict(torch.load(resume_path, map_location='cpu'))
-model.learning_rate = learning_rate
-model.sd_locked = sd_locked
-model.only_mid_control = only_mid_control
+model.load_state_dict(torch.load(args.resume_path, map_location='cpu'))
+model.learning_rate = 1e-5  # Set learning rate here if not using argparse for this
 
+# Dataset and DataLoader
+dataset = MyDataset(args.data_dir)
+dataloader = DataLoader(dataset, num_workers=0, batch_size=4, shuffle=True)  # Set batch size here if not using argparse for this
 
-# Misc
-dataset = MyDataset()
-dataloader = DataLoader(dataset, num_workers=0, batch_size=batch_size, shuffle=True)
-logger = ImageLogger(batch_frequency=logger_freq)
-
-
-trainer = pl.Trainer(accelerator='gpu', precision=16, callbacks=[logger, checkpoint_callback], max_epochs=10)
+# Trainer configuration
+trainer = pl.Trainer(accelerator='gpu', precision=16, callbacks=[checkpoint_callback], max_epochs=10)
 trainer.fit(model, dataloader)
-
-
