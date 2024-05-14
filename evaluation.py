936b4e5a220ea5d9d6eda00e08811634743b8252
Vittal Thirumalai <vittalt@mit.edu>
2024-05-13 20:02:59 -0400

    Updated evaluation

--- evaluation.py
+++ evaluation.py
@@ -39,20 +39,27 @@ pretrained_path = os.path.join(current_dir, "control_sd21_ini.ckpt")
 coco_dir = os.path.join(current_dir, "datasets/coco/")
 weights_dir = os.path.join(current_dir, "trainedweights/")
 
-eval_metrics_dir = os.path.join(
-    current_dir, "evalmetrics/"
-)  # location of trained weights to be saved
+eval_dir = os.path.join(current_dir, "eval/")
+eval_logs_dir = os.path.join(eval_dir, "logs/")
+eval_metrics_dir = os.path.join(eval_dir, "metrics/")
+eval_img_pred_weight_dir = os.path.join(eval_dir, "images/predicted/", args.weightName)
+os.makedirs(eval_dir, exist_ok=True)
+os.makedirs(eval_logs_dir, exist_ok=True)
 os.makedirs(eval_metrics_dir, exist_ok=True)
-
+os.makedirs(eval_img_pred_weight_dir, exist_ok=True)
 
 N = 1
 ddim_steps = 50
 
 device_name = "cuda" if torch.cuda.is_available() else "cpu"
 device = torch.device(device_name)
+print('1')
 model = create_model('./models/cldm_v21.yaml').to(device)
+print('2')
 model.load_state_dict(load_state_dict(os.path.join(weights_dir, args.weightName), location=device_name))
+print('3')
 ddim_sampler = DDIMSampler(model)
+print('4')
 
 TRAIN_RATIO, VAL_RATIO, TEST_RATIO = 0.4, 0.1, 0.5
 
@@ -110,11 +117,11 @@ def process_image_pair(img_path, label_path):
     label = cv2.cvtColor(label, cv2.COLOR_BGR2RGB)
     # label = cv2.resize(label, (predicted.shape[1], predicted.shape[0]))
     mse = np.mean((predicted - label) ** 2)
-    ssim1_value = structural_similarity(predicted, label, multichannel=True)
+    ssim1_value = structural_similarity(predicted, label, channel_axis = -1)
     ssim_value = ssim(predicted, label)
     fsim_value = fsim(predicted, label)
     
-    return mse, ssim1_value, ssim_value, fsim_value
+    return predicted, mse, ssim1_value, ssim_value, fsim_value
 
 def evaluate(input_paths, label_paths):
     # test_start = 4000 + 1000
@@ -126,20 +133,40 @@ def evaluate(input_paths, label_paths):
     # with ThreadPoolExecutor(max_workers=os.cpu_count()) as executor:
     #     results = executor.map(process_image_pair, input_paths, label_paths)
 
-    for input_path, label_path in zip(input_paths, label_paths):
-        mse, ssim1_value, ssim_value, fsim_value = process_image_pair(input_path, label_path)
+    for i, (img_path, label_path) in enumerate(zip(input_paths, label_paths)):
+        predicted, mse, ssim1_value, ssim_value, fsim_value = process_image_pair(img_path, label_path)
         mse_scores.append(mse)
         ssim1_scores.append(ssim1_value)
         ssim_scores.append(ssim_value)
         fsim_scores.append(fsim_value)
+
+        if i % 100 == 0:
+            image_save_path = os.path.join(eval_img_pred_weight_dir, f"{os.path.basename(img_path)}")
+            cv2.imwrite(image_save_path, cv2.cvtColor(predicted, cv2.COLOR_RGB2BGR))
+            eval_logs_file_path = os.path.join(eval_logs_dir, f'{args.weightName}.txt')
+            with open(eval_logs_file_path, "a") as log_file:
+                log_file.write(f"{i}: {os.path.basename(img_path)}, {os.path.basename(label_path)}\n")
+            
+            avg_mse = np.mean(mse_scores)
+            avg_ssim1 = np.mean(ssim1_scores)
+            avg_ssim = np.mean(ssim_scores)
+            avg_fsim = np.mean(fsim_scores)
+            eval_metrics_file_path = os.path.join(eval_metrics_dir, f'{args.weightName}.txt')
+            with open(eval_metrics_file_path, 'a') as file:
+                file.write(f"i: {i}\n")
+                file.write(f"Average MSE: {avg_mse}\n")
+                file.write(f"Average SSIM1: {avg_ssim1}\n")
+                file.write(f"Average SSIM: {avg_ssim}\n")
+                file.write(f"Average FSIM: {avg_fsim}\n")
 
     avg_mse = np.mean(mse_scores)
     avg_ssim1 = np.mean(ssim1_scores)
     avg_ssim = np.mean(ssim_scores)
     avg_fsim = np.mean(fsim_scores)
 
-    metrics_file_path = os.path.join(eval_metrics_dir, f'{args.weightName}.txt')
-    with open(metrics_file_path, 'w') as file:
+    eval_metrics_file_path = os.path.join(eval_metrics_dir, f'{args.weightName}.txt')
+    with open(eval_metrics_file_path, 'a') as file:
+        file.write(f" ----- OVERALL ----- ")
         file.write(f"Average MSE: {avg_mse}\n")
         file.write(f"Average SSIM1: {avg_ssim1}\n")
         file.write(f"Average SSIM: {avg_ssim}\n")
@@ -150,6 +177,7 @@ def evaluate(input_paths, label_paths):
 
 # source = os.path.join(coco_dir, 'source/')
 # target = os.path.join(coco_dir, 'target/')
+# eval = evaluate(source, target, 1000)
 
 print('start')
 input_paths, label_paths = get_data_paths(coco_dir, 10)
